f733f10cad0f1571ad0ad4142aa426c069132802
manishladkat29 <49076228+manishladkat29@users.noreply.github.com>
2020-06-23 06:55:45 -0500

    nodejs_database

---
+++ app.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "admin",
+  password: "admin"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
